9a531b3086d33f764a72889a39075853122536f1
balrajselva <balraj.selvaraju@gmail.com>
2019-11-14 18:18:54 -0500

    Fixed compilation errors

--- revamp-service/src/main/java/com/revamp/core/model/SchoolRegFormModel.java
+++ revamp-service/src/main/java/com/revamp/core/model/SchoolRegFormModel.java
@@ -2,7 +2,7 @@ package com.revamp.core.model;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Data;
-//@Data
+@Data
 public class SchoolRegFormModel {
 
     private String payload;
