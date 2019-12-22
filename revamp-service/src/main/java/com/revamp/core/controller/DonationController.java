e97c759861b9457a994b489bd03ed55f98aafcaf
srijkay <36108000+srijkay@users.noreply.github.com>
2019-12-21 23:05:18 -0500

    Update

--- revamp-service/src/main/java/com/revamp/core/controller/DonationController.java
+++ revamp-service/src/main/java/com/revamp/core/controller/DonationController.java
@@ -1,17 +1,24 @@
 package com.revamp.core.controller;
 
+import java.security.SecureRandom;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revamp.core.model.Donation;
+import com.revamp.core.model.DonationUser;
+import com.revamp.core.model.User;
 import com.revamp.core.service.DonationService;
+import com.revamp.core.service.DonationUserService;
 
 /**
  * 
@@ -23,6 +30,9 @@ public class DonationController {
 
 	@Autowired
 	private DonationService donationService;
+	
+	@Autowired
+	private DonationUserService donationUserService;
 
 	/**
 	 * 
@@ -47,5 +57,86 @@ public class DonationController {
 	public ResponseEntity<Donation> getDonation(@PathVariable("id") long id) {
 		return ResponseEntity.ok().body(donationService.get(id));
 	}
+	
+	
+	@PostMapping("/donate/save")
+	public ResponseEntity<DonationUser> saveUser(@RequestBody DonationUser donation) {
+		DonationUser donationResponse = donationUserService.save(donation);
+		return ResponseEntity.ok().body(donationResponse);
+	}
+	
+	/**
+	 * 
+	 * @param donation Id
+	 * @return donation
+	 */
+	@PostMapping("/donate/findByEmailId")
+	public String findEmail(@RequestBody DonationUser donation) {
+		String status = "";
+		
+		DonationUser user=null;
+		try {
+			user = donationUserService.findByEmailAddress(donation.getEmailAddress());
+			
+		if(user == null) {
+			status = "FAILURE";
+		}
+		else {
+			status = "SUCCESS";
+		}
+		}catch(Exception e) {
+				status = "SUCCESS";
+		}
+		
+		return status;
+	}
+	
+	/**
+	 * 
+	 * @param donation Id
+	 * @return donation
+	 */
+	@PostMapping("/donate/findDonationUser")
+	public ResponseEntity<DonationUser> findDonationUser(@RequestBody DonationUser donation) {
+		
+		DonationUser user = donationUserService.
+				findByEmailAddressPassword(donation.getEmailAddress(),donation.getPassword());
+		
+		if(user == null) {
+			DonationUser userDonation = new DonationUser();
+			userDonation.setEmailAddress("email");
+			
+			 return new ResponseEntity<DonationUser>(userDonation,HttpStatus.OK);
+		}
+		
+		return ResponseEntity.ok().body(user);
+	}
+	
+	/**
+	 * 
+	 * @param donation Id
+	 * @return donation
+	 */
+	@PostMapping("/donate/paymentDonation")
+	public ResponseEntity<Donation> savePaymentUser(@RequestBody Donation donation) {
+		
+		String trackIdGenerator =  "PTHL"+ generateTrackNumber();
+		
+		donation.setTracking_id(trackIdGenerator);
+		
+		Donation user = donationService.savePaymentUser(donation);
+		return ResponseEntity.ok().body(user);
+	}
+	
+	private  String generateTrackNumber() {
+	
+	 String result = new SecureRandom().ints(0,36)
+	            .mapToObj(i -> Integer.toString(i, 36))
+	            .map(String::toUpperCase).distinct().limit(8).collect(Collectors.joining())
+	            ;
+	 return result;
+	}
+
+	
 
 }
