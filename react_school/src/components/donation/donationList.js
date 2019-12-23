7211f2d04ea52eb8903eb4bd02b2fa957c6efb33
srijkay <36108000+srijkay@users.noreply.github.com>
2019-12-22 23:32:28 -0500

    Update

--- react_school/src/components/donation/donationList.js
+++ react_school/src/components/donation/donationList.js
@@ -23,12 +23,12 @@ class DonationList extends Component {
     }
 
     render() {
-       
+
         const styleDonation = {
             position: "absolute",
             left: "0px",
             top: "0px",
-        transform: "translate3d(0px,0px,0px)"
+            transform: "translate3d(0px,0px,0px)"
         };
         const styleDonationP = {
             overflow: "hidden",
@@ -45,24 +45,31 @@ class DonationList extends Component {
             <div className="row">
                 <div className="projects isotope" style={styleDonationP}>
                     {this.props.donationList.map(donationLists => (
-                    
+
                         <div className="span3 element isotope-item" style={styleDonation} >
                             <div className="hover_img">
-                                <a className=""><img src={donationLists.proofOfIds.files[0]}  alt="" style={styleChildren}></img></a>
+                                <a className=""><img src={donationLists.proofOfIds.files[0]} alt="" style={styleChildren}></img></a>
                                 <span className="portfolio_zoom"><a href={donationLists.proofOfIds.files[0]} rel="prettyPhoto[portfolio1]"></a></span>
                                 <span className="portfolio_link"><a href="show_one_school_details_for_donation.html">View item</a></span>
                             </div>
                             <div className="item_description">
                                 <h6><a href="show_one_school_details_for_donation.html">{donationLists.schoolInfo.schoolName}</a></h6>
-                                <div className="descr">Needs bathroom, water facility &amp; Digital Boards</div>
+                                <div className="descr">
+                                    Needs 
+                                {donationLists.requirements.map((value, index) =>
+                     <div>{value.quantity} {value.assetName} </div>
+                    )}
+                   
+
+                                </div>
                                 <div className="descr">Click Here to  <button onClick={e => this.toDonate(donationLists.id)}>
                                     Donate
                                  </button>
-                                 </div>
+                                </div>
                             </div>
 
                         </div>
-                        
+
                     ))}
                 </div>
                 <div className="clear"></div>
