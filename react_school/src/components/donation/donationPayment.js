0007ac57caac5d46be2250fe288b0802034a3d6d
srijkay <36108000+srijkay@users.noreply.github.com>
2019-12-22 23:33:20 -0500

    Update

--- react_school/src/components/donation/donationPayment.js
+++ react_school/src/components/donation/donationPayment.js
@@ -1,59 +1,60 @@
-import React , {Component}  from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import axios from "axios";
 class DonationPayment extends Component {
-constructor(props){
-super(props);
-this.submitClick = this.submitClick.bind(this);
-}
-
-submitClick= (e) =>  {
-  let projectPayload = {
-    project :{
-      projectId :this.props.history.location.user.projectId
-    }
-  }
-  let donationUserIdPL = {
-    donationuserid :{
-      donationuserid :this.props.history.location.user.donationuserid
-    }
+  constructor(props) {
+    super(props);
+    this.submitClick = this.submitClick.bind(this);
   }
 
-let paymentUserPayload = {
-amount:this.props.history.location.user.contribution,
-paymentStatus:'PENDING',
-paymentMode:'CASH'
-  };
+  submitClick = (e) => {
+    let projectPayload = {
+      project: {
+        projectId: this.props.history.location.user.projectId
+      }
+    }
+    let donationUserIdPL = {
+      donationuserid: {
+        donationuserid: this.props.history.location.user.donationuserid
+      }
+    }
 
-  var donationUserPayload = Object.assign(projectPayload, donationUserIdPL,paymentUserPayload);
-            
-  axios.post('http://localhost:8088/puthuyir/donate/paymentDonation', donationUserPayload, { headers: { 'Accept': 'application/json' } })
-  .then(response => {
-     this.props.history.push({
-       pathname: '/donationPaymentConfirmation',
-       user: response.data,
-      });
-    })
-  
-}
-render () {
-console.log(this.props.history);
-  return(
-       
-<div className="page_container">
-    	<div className="breadcrumb">
-        	<div className="wrap">
-            	<div className="container">
-                    <a href="index.html">Summary before </a><span>/</span>3rd Party Site(Payment Gateway)
+    let paymentUserPayload = {
+      amount: this.props.history.location.user.contribution,
+      paymentStatus: 'PENDING',
+      paymentMode: 'CASH'
+    };
+
+    var donationUserPayload = Object.assign(projectPayload, donationUserIdPL, paymentUserPayload);
+
+    axios.post('http://localhost:8088/puthuyir/donate/paymentDonation', donationUserPayload, { headers: { 'Accept': 'application/json' } })
+      .then(response => {
+        this.props.history.push({
+          pathname: '/donationPaymentConfirmation',
+          user: response.data,
+        });
+      })
+
+  }
+  render() {
+    const requirements = this.props.history.location.user.requirements;
+
+    return (
+
+      <div className="page_container">
+        <div className="breadcrumb">
+          <div className="wrap">
+            <div className="container">
+              <a href="index.html">Summary before </a><span>/</span>3rd Party Site(Payment Gateway)
                 </div>
-            </div>
+          </div>
         </div>
-    	<div className="wrap">
-        	<div className="container">
-                <section>
-                    <div className="row">
-                        <div className="span12">
-                        <div className="row">
+        <div className="wrap">
+          <div className="container">
+            <section>
+              <div className="row">
+                <div className="span12">
+                  <div className="row">
                     <div className="span10">
                       <h2>Thanks &amp; Appreciate for your contributions.</h2><h3>Summary of Payment details</h3><table class="table table-bordered table-striped">
                         <colgroup>
@@ -66,15 +67,15 @@ console.log(this.props.history);
                               <lable>Donor First Name
                             </lable></td>
                             <td>
-                            {this.props.history.location.user.firstName}
-                            </td> 
+                              {this.props.history.location.user.firstName}
+                            </td>
                           </tr>
                           <tr>
                             <td>
                               <lable>Donor Last Name
                             </lable></td>
                             <td>
-                            {this.props.history.location.user.lastName} 
+                              {this.props.history.location.user.lastName}
                             </td>
                           </tr>
                           <tr>
@@ -86,43 +87,37 @@ console.log(this.props.history);
                             </td>
                           </tr>
                           <tr>
-                              <td>
-                                <lable>Donor Phone Number
+                            <td>
+                              <lable>Donor Phone Number
                               </lable></td>
-                              <td>
-                              {this.props.history.location.user.phoneNumber} 
-                              </td>
-                          </tr>
-                          <tr>
                             <td>
-                              <lable>School Name 
-                            </lable></td>
-                            <td>
-                              {this.props.history.location.user.schoolName} 
+                              {this.props.history.location.user.phoneNumber}
                             </td>
                           </tr>
                           <tr>
                             <td>
-                              <lable>Donoted Amount 
+                              <lable>School Name
                             </lable></td>
                             <td>
-                            {this.props.history.location.user.contribution} 
+                              {this.props.history.location.user.schoolName}
                             </td>
                           </tr>
                           <tr>
                             <td>
-                              <lable>Rquirement Description 
+                              <lable>Donoted Amount
                             </lable></td>
                             <td>
-                              Tables, Digital Boards, Chairs
+                              {this.props.history.location.user.contribution}
                             </td>
                           </tr>
                           <tr>
                             <td>
-                              <lable>Mode of Payment 
+                              <lable>Requirements Description
                             </lable></td>
                             <td>
-                              Card Payment / Mobile Payment / Wire Transfer
+                              {requirements.map((value, index) =>
+                                <li>Needs {value.quantity} {value.assetName}</li>
+                              )}
                             </td>
                           </tr>
                         </tbody>
@@ -131,17 +126,17 @@ console.log(this.props.history);
 
                   </div>
 
-                          <h3><button onClick={this.submitClick}>Link to Payment Gateway</button></h3>
+                  <h3><button onClick={this.submitClick}>Link to Payment Gateway</button></h3>
 
-                        </div>
-                    </div>
-                </section>
-            </div>
+                </div>
+              </div>
+            </section>
+          </div>
         </div>
-    </div>
-  
+      </div>
+
     );
-}
+  }
 
 }
 
