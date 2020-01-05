cd6745202b510fd54af61ecfb83bcd0117720c04
Balraj <balraj.selvaraju@gmail.com>
2020-01-05 13:18:17 -0500

    refactored quotation UI changes

--- react_admin/src/adminWebsite/components/quotation/addQuotation.js
+++ react_admin/src/adminWebsite/components/quotation/addQuotation.js
@@ -70,21 +70,33 @@ saveClicked=()=>{
             quotationPreparedBy:this.state.quotationPreparedBy,
             quantity:this.state.quantity,
             details:this.state.details,
-            cost:this.state.cost
+            cost:this.state.cost,
+            schoolId:this.props.location.school.schoolId
         }
         console.log(quotation);
         this.saveQuotation(quotation);
     }
 }
 
+cancelClicked=()=>{
+    this.props.history.push({
+        pathname:"/viewRequirements",
+        user:this.props.location.user,
+        school:this.props.location.school,
+        ...this.props
+    })    
+} 
+
 saveQuotation=(quotation)=>{
-    // this.setState({user:quotation,spinner:true});
     axios.post('http://localhost:6060/puthuyir/quotation',quotation)
     .then(res=>{
         console.log(res);
         this.props.history.push({
             pathname: '/viewRequirements',
-            quotation: res.data
+            quotation: res.data,
+            user:this.props.location.user,
+            school:this.props.location.school,
+            ...this.props            
         });
     })
 }
@@ -107,41 +119,24 @@ render() {
                 <div className="form-group has-feedback col-md-6">
                     <input type="text" className="form-control" id="companyName" value={this.state.companyName} placeholder="Enter Company Name" onChange={this.handleChange}/>                    
                     <span className="glyphicon glyphicon-user form-control-feedback" />
-                </div>
-                <div className="form-group has-feedback col-md-6">
                     <input type="text" className="form-control" id="address" value={this.state.address} placeholder="Enter Address" onChange={this.handleChange}/>                    
-                </div>
-                <div className="form-group has-feedback col-md-6">
                     <input type="text" className="form-control" id="city" value={this.state.city} placeholder="Enter City" onChange={this.handleChange}/>                    
-                </div>
-                <div className="form-group has-feedback col-md-6">
                     <input type="text" className="form-control" id="state" value={this.state.state} placeholder="Enter State" onChange={this.handleChange}/>                    
-                </div>
-                <div className="form-group has-feedback col-md-6">
                     <input type="text" className="form-control" id="pincode" value={this.state.pincode} placeholder="Enter Pincode" onChange={this.handleChange}/>                    
-                </div>  
-                <div className="form-group has-feedback col-md-6">
                     <input type="text" className="form-control" id="phone" value={this.state.phone} placeholder="Enter PhoneNumber" onChange={this.handleChange}/>                    
-                </div>                                                                                                             
+                </div>                                                                                                           
                 <div className="form-group has-feedback col-md-6">
                     <input type="text" className="form-control" id="quotationDate" value={this.state.quotationDate} placeholder="Enter Quotation Date" onChange={this.handleChange}/>                    
-                </div>
-                <div className="form-group has-feedback col-md-6">
                     <input type="text" className="form-control" id="quotationValidDate" value={this.state.quotationValidDate} placeholder="Enter Quotation ValidDate" onChange={this.handleChange}/>
-                </div>
-                <div className="form-group has-feedback col-md-6">
                     <input type="text" className="form-control" id="quotationPreparedBy" value={this.state.quotationPreparedBy} placeholder="Enter Quotation PreparedBy" onChange={this.handleChange}/>
-                </div>
-                <div className="form-group has-feedback col-md-6">
                     <input type="text" className="form-control" id="quantity" value={this.state.quantity} placeholder="Enter Quantity" onChange={this.handleChange}/>
-                </div>	
-                <div className="form-group has-feedback col-md-6">
                     <input type="text" className="form-control" id="details" value={this.state.details} placeholder="Enter Details" onChange={this.handleChange}/>
-                </div>	
-                <div className="form-group has-feedback col-md-6">
                     <input type="text" className="form-control" id="cost" value={this.state.cost} placeholder="Enter Cost" onChange={this.handleChange}/>
                 </div>	                                                					
             </div>
+            <div className="col-md-6">
+                    <button type="submit" className="btn btn-primary btn-block btn-flat" onClick={()=>this.cancelClicked()}>Cancel</button><br/>
+            </div>            
             <div className="col-md-6">
                     <button type="submit" className="btn btn-primary btn-block btn-flat" onClick={()=>this.saveClicked()}>Save</button><br/>
             </div>
