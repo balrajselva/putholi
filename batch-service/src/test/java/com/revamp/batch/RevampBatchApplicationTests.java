c296241077fc9e7ae2cb4ff640f4c1ba8c2914e7
arun-uk <arun.x.mohanan@gmail.com>
2020-05-17 00:54:12 +0100

    Refactor: Organized the project structure to have parent pom
    Dependency management
    Renamed the project artifacts
    Removed mysql dependency on some projects

---
+++ batch-service/src/test/java/com/revamp/batch/RevampBatchApplicationTests.java
@@ -0,0 +1,22 @@
+package com.revamp.batch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {RevampBatchApplication.class})
+@SpringBootTest(classes = { RevampBatchApplication.class })
+@ActiveProfiles("test")
+@TestPropertySource(locations = { "/application-test.properties" })
+public class RevampBatchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ email-service/src/test/java/com/revamp/start/RevampServiceEmailApplicationTests.java
@@ -0,0 +1,18 @@
+package com.revamp.start;
+
+import com.revamp.email.RevampServiceEmailApplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=RevampServiceEmailApplication.class)
+public class RevampServiceEmailApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
