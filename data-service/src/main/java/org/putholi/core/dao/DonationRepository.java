c5213aba314263238f380af05191c861fbf8346d
arunkumarPutholi <arunkumarchinnasamy23@gmail.com>
2020-06-20 10:12:54 +0530

    Backend @param fixes

--- data-service/src/main/java/org/putholi/core/dao/DonationRepository.java
+++ data-service/src/main/java/org/putholi/core/dao/DonationRepository.java
@@ -3,8 +3,9 @@ package org.putholi.core.dao;
 import org.putholi.core.model.Donation;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface DonationRepository extends CrudRepository<Donation, Long>,TrackDonationCustomRepository {
     @Query("FROM Donation d where d.orderId = :orderId")
-    Donation findByOrderId(String orderId);
+    Donation findByOrderId(@Param("orderId")String orderId);
 }
